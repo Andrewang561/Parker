2b3074d0e1f45eb76c7cf372bd9cb27adb02a976
andrewwang <andrewang562@gmail.com>
2026-07-30 23:44:27 -0700

    Created a ParkingLot DTO

---
+++ src/main/java/com/parker/dto/ParkingLot.java
@@ -0,0 +1,12 @@
+package com.parker.dto;
+
+public class ParkingLot {
+    
+    private double lat;
+    private double lng;
+
+    public ParkingLot(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
