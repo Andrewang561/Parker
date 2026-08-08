4a73fd8287a94d3220a8ce91c99db3e0adcfee82
andrewwang <andrewang562@gmail.com>
2026-08-07 17:38:46 -0700

    Initialized and built Spring's HTTP Client

--- src/main/java/com/parker/service/OSMService.java
+++ src/main/java/com/parker/service/OSMService.java
@@ -3,10 +3,19 @@ package com.parker.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
 import com.parker.dto.ParkingLot;
 
+@Service
 public class OSMService {
     
+    private final RestClient restClient;
+
+    public OSMService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.baseUrl("https://overpass-api.de").build();
+    }
     public List<ParkingLot> findParkingLots(double lat, double lng) {
         List<ParkingLot> lots = new ArrayList<>();
         return lots;
