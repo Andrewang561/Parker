1cf8ec08ffb82f1a3de9b6ba5cc8b699141b3a07
andrewwang <andrewang562@gmail.com>
2026-08-10 20:22:40 -0700

    Added Center DTO

---
+++ src/main/java/com/parker/dto/Center.java
@@ -0,0 +1,15 @@
+package com.parker.dto;
+
+public class Center {
+    
+    private double lat;
+    private double lng;
+
+    public double getLat() {
+        return this.lat;
+    }
+
+    public double getLng() {
+        return this.lng;
+    }
+}
