baf7376ff55034471250d1ee83be2aa7b5bc1cde
andrewwang <andrewang562@gmail.com>
2026-08-10 20:28:56 -0700

    Created OverPassResponse DTO

---
+++ src/main/java/com/parker/dto/OverPassResponse.java
@@ -0,0 +1,12 @@
+package com.parker.dto;
+
+import java.util.List;
+
+public class OverPassResponse {
+    
+    private List<OverPassElement> elements;
+
+    public List<OverPassElement> getElements() {
+        return this.elements;
+    }
+}
