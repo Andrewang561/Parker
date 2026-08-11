89cc804d619fde2b85eef1f552ce00429b24d8a3
andrewwang <andrewang562@gmail.com>
2026-08-10 20:27:03 -0700

    Created OverPassElement DTO

---
+++ src/main/java/com/parker/dto/OverPassElement.java
@@ -0,0 +1,25 @@
+package com.parker.dto;
+
+public class OverPassElement {
+    
+    private String type;
+    private long id;
+    private Center center;
+    private Tags tags;
+
+    public String getType() {
+        return this.type;
+    }
+
+    public long getID() {
+        return this.id;
+    }
+
+    public Center getCenter() {
+        return this.center;
+    }
+
+    public Tags getTags() {
+        return this.tags;
+    }
+}
