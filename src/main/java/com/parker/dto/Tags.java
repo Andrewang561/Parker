d11ae2e61edc11eff51b017acbdcdb93d6f43759
andrewwang <andrewang562@gmail.com>
2026-08-10 20:23:57 -0700

    Created Tags DTO

---
+++ src/main/java/com/parker/dto/Tags.java
@@ -0,0 +1,15 @@
+package com.parker.dto;
+
+public class Tags {
+    
+    private String amenity;
+    private String parking;
+
+    public String getAmenity() {
+        return this.amenity;
+    }
+
+    public String getParking() {
+        return this.parking;
+    }
+}
