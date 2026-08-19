7986db2b9aafd1916e2a1ebc3502c0b3a3ce5c11
andrewwang <andrewang562@gmail.com>
2026-08-18 17:42:59 -0700

    Allow front-end origin to fetch

--- src/main/java/com/parker/controller/ParkingController.java
+++ src/main/java/com/parker/controller/ParkingController.java
@@ -1,5 +1,6 @@
 package com.parker.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,7 @@ import com.parker.dto.OverPassResponse;
 import com.parker.dto.ParkingResponse;
 import com.parker.service.ParkingService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("api")
 public class ParkingController {
