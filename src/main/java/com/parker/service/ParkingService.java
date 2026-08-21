c351c2b3b25cabbc0183299f99836b66a48605fe
andrewwang <andrewang562@gmail.com>
2026-08-20 22:25:47 -0700

    Added basic score calculation based on number of elements in the response

--- src/main/java/com/parker/service/ParkingService.java
+++ src/main/java/com/parker/service/ParkingService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.parker.dto.OverPassElement;
 import com.parker.dto.OverPassResponse;
 import com.parker.dto.ParkingLot;
 import com.parker.dto.ParkingResponse;
@@ -18,9 +19,18 @@ public class ParkingService {
     }
 
     public ParkingResponse getDifficulty(double lat, double lng) {
-        int score = 0;
-        String difficulty = "";
-        OverPassResponse parkingLots= osmService.findParkingLots(lat, lng);
+        int score = 100;
+        String difficulty = "HARD";
+        OverPassResponse parkingLotResponse = osmService.findParkingLots(lat, lng);
+        List<OverPassElement> parkingLots = parkingLotResponse.getElements();
+        for (int i = 0; i < parkingLots.size(); i++) {
+            score -= 10;
+        }
+        if (score > 30 && score < 70) {
+            difficulty = "MEDIUM";
+        } else if (score < 20) {
+            difficulty = "EASY";
+        }
         return new ParkingResponse(difficulty, score);
     }
 }
